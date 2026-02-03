dfc75ab63188ec99bc22528eabcf6a2f07daa6d3
Uttkarsh <rupareluttkarsh2309@gmail.com>
2026-02-04 04:31:02 +0530

    updated url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/students": {
-        target: "http://localhost:3000",
+        target: "http://172.16.19.129",
         changeOrigin: true,
         secure: false,
       },
